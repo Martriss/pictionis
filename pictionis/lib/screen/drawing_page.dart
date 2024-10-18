e2d4607ff6d93cbbad672d31adc05f557fad1717
Martin Risser <mrisser@deezer.com>
2024-10-18 16:52:26 +0200

    Update drawing page to hold relevant controller/state

--- pictionis/lib/screen/drawing_page.dart
+++ pictionis/lib/screen/drawing_page.dart
@@ -1,23 +1,62 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:pictionis/models/game_state.dart';
+import 'package:pictionis/firebase_draw_controller.dart';
+import 'package:pictionis/models/drawing_state.dart';
 import 'package:pictionis/widgets/draw_canvas.dart';
 import 'package:pictionis/widgets/draw_controller.dart';
 import 'package:provider/provider.dart';
 
-class DrawingPage extends StatelessWidget {
+class DrawingPage extends StatefulWidget {
   const DrawingPage({super.key});
 
   @override
+  State<DrawingPage> createState() => _DrawingPageState();
+}
+
+class _DrawingPageState extends State<DrawingPage> {
+  late final DrawingState _drawingState;
+  late final FirebaseDrawController? _firebaseDrawController;
+
+  @override
+  void initState() {
+    super.initState();
+    _drawingState = DrawingState();
+    final firestore = context.read<FirebaseFirestore?>();
+    if (firestore == null) {
+      log("Firestore instance wasn't provided. "
+          'Running without _firestoreController.');
+    } else {
+      _firebaseDrawController = FirebaseDrawController(
+        instance: firestore,
+        drawingState: _drawingState,
+        roomID: "TEST",
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _firebaseDrawController?.dispose();
+    _drawingState.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const DrawController(
-        child: DrawCanvas(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.read<GameState>().clearPaintEvents();
-        },
-        child: const Icon(Icons.clear),
+    return Provider(
+      create: (context) => _drawingState,
+      child: Scaffold(
+        body: const DrawController(
+          child: DrawCanvas(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _drawingState.clearPaintEvents();
+          },
+          child: const Icon(Icons.clear),
+        ),
       ),
     );
   }
