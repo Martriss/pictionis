91b0026ce5756869f1af1bb22ac2625c2878c93b
Martin Risser <mrisser@deezer.com>
2024-10-24 15:27:53 +0200

    Fix events order in firebase

--- pictionis/lib/firebase_draw_controller.dart
+++ pictionis/lib/firebase_draw_controller.dart
@@ -2,21 +2,16 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:logging/logging.dart';
 import 'package:pictionis/models/drawing_state.dart';
 import 'package:pictionis/models/paint_event.dart';
 
 // https://docs.flutter.dev/cookbook/games/firestore-multiplayer#4-create-a-firestore-controller-class
 class FirebaseDrawController {
-  static final _log = Logger('FirestoreController');
-
   final FirebaseFirestore instance;
 
   final DrawingState drawingState;
   final String roomID;
 
-  /// For now, there is only one match. But in order to be ready
-  /// for match-making, put it in a Firestore collection called matches.
   late final _roomRef = instance.collection('rooms').doc(roomID);
 
   late final _drawEventsRef = _roomRef.collection('drawEvents');
@@ -29,30 +24,30 @@ class FirebaseDrawController {
       {required this.instance,
       required this.drawingState,
       required this.roomID}) {
-    // Subscribe to the remote changes (from Firestore).
     _roomFirestoreSubscription = _drawEventsRef.snapshots().listen((snapshot) {
-      if (snapshot.docs.isNotEmpty) {
-        drawingState.addFirebasePaintEvent(
-            PaintEvent.fromJson(snapshot.docs.last.data()));
+      if (snapshot.docChanges.isNotEmpty) {
+        for (var docChange in snapshot.docChanges) {
+          final data = docChange.doc.data();
+          if (data != null) {
+            drawingState.addFirebasePaintEvent(PaintEvent.fromJson(data));
+          }
+        }
       }
     });
 
-    // Subscribe to the local changes in game state.
+//TODO: Add user id to event
     _roomLocalSubscription = drawingState.localChanges.listen((_) {
       if (drawingState.events.isNotEmpty) {
-        log(drawingState.events.last.toJson().toString());
-        _drawEventsRef.add(drawingState.events.last.toJson());
+        final lastEvent = drawingState.events.last.toJson();
+        lastEvent['timestamp'] = FieldValue.serverTimestamp();
+        _drawEventsRef.add(lastEvent);
       }
     });
-
-    log('Initialized');
   }
 
   void dispose() {
     _roomFirestoreSubscription?.cancel();
     _roomLocalSubscription?.cancel();
-
-    _log.fine('Disposed');
   }
 }
 
