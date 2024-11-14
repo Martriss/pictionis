9ad8870296b4c38ba627033dac18ebe0d3c60d86
mouton <romain.savary@etu.u-pec.fr>
2024-11-14 21:45:22 +0100

    feat(view): better page

--- pictionis/lib/screen/login_page.dart
+++ pictionis/lib/screen/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pictionis/service/auth_service.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,12 +12,29 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  final loginPage = true;
+
+  Widget _title() {
+    var title = 'CONNEXION';
+    if (!loginPage) title = 'INSCRIPTION';
+    return Text(
+      title,
+      style: GoogleFonts.luckiestGuy(
+        fontSize: 64,
+        // color: Color.black,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Connexion"),
+        title: Text("Pictionis",
+            style: GoogleFonts.luckiestGuy(
+              fontSize: 24,
+              // color: Color.black,
+            )),
       ),
       body: Center(
         child: Padding(
@@ -34,27 +52,25 @@ class _LoginPageState extends State<LoginPage> {
                   decoration: const InputDecoration(hintText: "Mot de passe"),
                 ),
                 ElevatedButton(
-                  onPressed: () {
-                    if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
-                      Auth().signInWithEmailAndPassword(
-                        email: emailController.text, 
-                        password: passwordController.text
-                      );
-                    }
-                  }, 
-                  child: const Text("Connexion")
-                ),
+                    onPressed: () {
+                      if (emailController.text.isNotEmpty &&
+                          passwordController.text.isNotEmpty) {
+                        Auth().signInWithEmailAndPassword(
+                            email: emailController.text,
+                            password: passwordController.text);
+                      }
+                    },
+                    child: const Text("Connexion")),
                 TextButton(
-                  onPressed: () {
-                    if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
-                      Auth().createUserWithEmailAndPassword(
-                        email: emailController.text, 
-                        password: passwordController.text
-                      );
-                    }
-                  },
-                  child: const Text("s'inscrire")
-                ),
+                    onPressed: () {
+                      if (emailController.text.isNotEmpty &&
+                          passwordController.text.isNotEmpty) {
+                        Auth().createUserWithEmailAndPassword(
+                            email: emailController.text,
+                            password: passwordController.text);
+                      }
+                    },
+                    child: const Text("s'inscrire")),
               ],
             ),
           ),
