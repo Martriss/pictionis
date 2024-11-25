b8dbeddd8a172661fe2e9250766a57da55f07365
mouton <romain.savary@etu.u-pec.fr>
2024-11-25 21:13:23 +0100

    fix: clean code

--- pictionis/lib/screen/login_register_page.dart
+++ pictionis/lib/screen/login_register_page.dart
@@ -180,11 +180,9 @@ class _LoginRegisterPageState extends State<LoginRegisterPage> {
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/background.jpg'),
-            // image: NetworkImage("https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp"),
             fit: BoxFit.cover
           ),
         ),
-        // color: Colors.red,
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
